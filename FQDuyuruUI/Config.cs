e90aaa4a8404c932af15949f782d7a1b80f23e7a
SydeFQ <sydefq@gmail.com>
2021-11-12 01:54:44 +0200

    Add project files.

---
+++ FQDuyuruUI/Config.cs
@@ -0,0 +1,26 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FQDuyuruUI
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public void LoadDefaults()
+        {
+            effectID = 45896;
+            beklemeAraligi = 30f;
+            Mesajlar = new List<string>() { 
+                "deneme1",
+                "deneme2"
+            };
+        }
+
+        public float beklemeAraligi;
+        public ushort effectID;
+        public List<String> Mesajlar;
+    }
+}
---
+++ FQDuyuruUI/Main.cs
@@ -0,0 +1,56 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FQDuyuruUI
+{
+    public class Main : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            StartCoroutine(duyuruRutin());
+        }
+
+        protected override void Unload()
+        {
+            StopAllCoroutines();
+        }
+
+        public IEnumerator<WaitForSeconds> duyuruRutin()
+        {
+            while (true)
+            {
+                duyuru();
+                yield return new WaitForSeconds(Configuration.Instance.beklemeAraligi);
+            }
+        }
+
+        public void duyuru()
+        {
+            if(duyuruCount >= Configuration.Instance.Mesajlar.Count)
+            {
+                duyuruCount = 0;
+            }
+
+            String msg = Configuration.Instance.Mesajlar[duyuruCount];
+
+            foreach(var steamPlayer in Provider.clients)
+            {
+                var player = UnturnedPlayer.FromSteamPlayer(steamPlayer);
+                EffectManager.sendUIEffect(Configuration.Instance.effectID, 78, player.SteamPlayer().transportConnection, true, $"{msg}");
+            }
+            duyuruCount++;
+            
+
+        }
+
+        private int duyuruCount = 0;
+
+    }
+}
